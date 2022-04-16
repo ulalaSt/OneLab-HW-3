37ab27319e06c0d2a85c4556d84abd3433720193
Ulan Seitkali <ulan.seitkali@nu.edu.kz>
2022-04-17 01:15:48 +0600

    Initial Commit

---
+++ OneLab-HW3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OneLab-HW3
+//
+//  Created by user on 17.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
